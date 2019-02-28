7b325b0bf1369616afa0a9c74115d64930c22f40
Vinicius Stock <vinistock@users.noreply.github.com>
2019-02-28 16:28:39 -0300

    Create preview api (#9)
    * Create notifications preview API
    
    The preview endpoint returns the total amount of notifications
    for a given subscriber and the newest notification.
    
    * Fix rubocop issues

--- test/controllers/notifications_controller_test.rb
+++ test/controllers/notifications_controller_test.rb
@@ -4,22 +4,35 @@ require "test_helper"
 
 module Correspondent
   class NotificationsControllerTest < ActionDispatch::IntegrationTest
+    def setup
+      @subscriber = User.create!(name: "user", email: "user@email.com")
+      publisher = Purchase.create!(name: "purchase", user: @subscriber)
+
+      @notification1 = Correspondent::Notification.create_for!(publisher, :user, :purchase)
+      @notification2 = Correspondent::Notification.create_for!(publisher, :user, :purchase)
+      @notification3 = Correspondent::Notification.create_for!(publisher, :user, :purchase)
+    end
+
     test "GET index" do
-      subscriber = User.create!(name: "user", email: "user@email.com")
-      publisher = Purchase.create!(name: "purchase", user: subscriber)
-
-      notification1 = Correspondent::Notification.create_for!(publisher, :user, :purchase)
-      notification2 = Correspondent::Notification.create_for!(publisher, :user, :purchase)
-      notification3 = Correspondent::Notification.create_for!(publisher, :user, :purchase)
-
-      get "/correspondent/user/#{subscriber.id}/notifications", headers: { accept: "application/json" }
+      get "/correspondent/user/#{@subscriber.id}/notifications", headers: { accept: "application/json" }
 
       body = JSON.parse(response.body)
 
       assert_response :ok
-      assert_equal body[0]["id"], notification3.id
-      assert_equal body[1]["id"], notification2.id
-      assert_equal body[2]["id"], notification1.id
+      assert_equal body[0]["id"], @notification3.id
+      assert_equal body[1]["id"], @notification2.id
+      assert_equal body[2]["id"], @notification1.id
+      assert_includes response.headers, "eTag"
+    end
+
+    test "GET preview" do
+      get "/correspondent/user/#{@subscriber.id}/notifications/preview", headers: { accept: "application/json" }
+
+      body = JSON.parse(response.body)
+
+      assert_response :ok
+      assert_equal body["notification"]["id"], @notification3.id
+      assert_equal body["count"], 3
       assert_includes response.headers, "eTag"
     end
   end
